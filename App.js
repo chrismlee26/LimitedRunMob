3df139a38de8e82b2fbf32f635b7066241753bd3
CHRIS_LEE <chrismlee87@gmail.com>
2021-03-16 23:38:48 -0700

    boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, Alert, Button, Platform, Dimensions } from 'react-native';
 
 export default function App() {
+  console.log(require('./app/assets/icon.png'))
   return (
-    <View style={styles.container}>
-      <Text style={{color: 'white'}}>Limited Run... Mobile</Text>
-      <StatusBar style="auto" />
-    </View>
+    // styles.container is a plain JS object. Use {{}} for inline
+    // <SafeAreaView style={styles.container}>
+
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <StatusBar style="light" />
+      <Text style={{ color: 'white', fontSize: 35, letterSpacing: 5, fontFamily: 'Baskerville' }}>limited run</Text>
+      <Text>1234567</Text>
+      {/* <TouchableHighlight onPress={() => console.log('Image tapped')}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300" 
+          }}  
+        />
+      </TouchableHighlight> */}
+      <Button 
+        color="orange"
+        title="CLick Me" 
+          // onPress={() => Alert.alert("My Title", "My Message", [
+          //   {text: "Yes", onPress: () => console.log('Yes')},
+          //   {text: "No", onPress: () => console.log('No')},
+          // ])}
+        // Alert.prompt is iOS only
+        onPress={() => 
+          Alert.prompt("My Title", "My Message", text => console.log(text))
+        }
+      />
+    </SafeAreaView>
   );
 }
 
+const containerStyle = { backgroundColor: "#727272" };
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: '#727272',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
+
   },
 });
